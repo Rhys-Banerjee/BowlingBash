c35f712a751e7bc84ffcc1d26804f5f97ec7a6f7
Rhys <rhys.banerjee@gmail.com>
2022-07-07 23:50:23 -0400

    New stuffs

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -4,8 +4,8 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const MAX_SPEED = 700
-const ACCELERATION = 150
+const MAX_SPEED = 1200
+const ACCELERATION = 800
 const FRICTION = 50
 
 var localScore = 0
@@ -20,8 +20,8 @@ func _has_collided():
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
@@ -30,12 +30,3 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
-	
-	for index in get_slide_count():
-		var collision = get_slide_collision(index)
-		if collision.collider.is_in_group("Pin") and !visited.has(collision.collider.name):
-			localScore += 1
-			emit_signal("increase_Player_score")
-			if !visited.has(collision.collider.name):
-				visited.append(collision.collider.name)
-			print(localScore)
