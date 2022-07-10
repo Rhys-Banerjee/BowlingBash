3499823a75d2e4c8509e53cb452f50d3b6052e68
Rhys <rhys.banerjee@gmail.com>
2022-07-09 21:59:55 -0400

    Stuff'

---
+++ Scenes/Main.gd
@@ -0,0 +1,8 @@
+extends Node2D
+var playerScene = preload("res://scenes/ball.tscn")
+var spawnPosition = Vector2.ZERO
+var currentPlayerNode = null
+
+func _ready():
+	pass # Replace with function body.
+
