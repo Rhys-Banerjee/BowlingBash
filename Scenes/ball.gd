a69f58a4973861d0c859ada8efec2d89de5c5a70
Rhys Banerjee <urb@duke.edu>
2022-12-24 22:31:35 -0500

    Got a jump in the game

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -28,7 +28,9 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	input_vector = input_vector.normalized()
-	
+	if Input.is_action_pressed("space"):
+		$AnimationPlayer.play("jump")
+		
 	if input_vector != Vector2.ZERO:
 		velocity += input_vector * ACCELERATION * delta
 		velocity = velocity.clamped(MAX_SPEED)
