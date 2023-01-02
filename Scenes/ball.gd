3f48b016c711c740be83f77c8dd7dba8544ae162
Rhys <rhys.banerjee@gmail.com>
2023-01-01 19:19:26 -0500

    Pixel assets used for bowling ball

--- Scenes/ball.gd
+++ Scenes/ball.gd
@@ -8,6 +8,11 @@ const FRICTION = 50
 
 onready var isOnPlatform = true
 onready var isDead = false
+
+onready var ballAnimations = $ballAnimations #animationPlayer
+onready var ballMovements = $ballMovement #animationTree
+onready var animationState = ballMovements.get("parameters/playback")
+
 var localScore = 0
 var visited = []
 signal increase_Player_score
@@ -44,9 +49,13 @@ func _physics_process(delta):
 	if hasExitedPlatform and $jump_timer.time_left == 0:
 		emit_signal("died")
 	if input_vector != Vector2.ZERO:
+		ballMovements.set("parameters/Idle/blend_position", input_vector)
+		ballMovements.set("parameters/Rolling/blend_position", input_vector)
+		animationState.travel("Rolling")
 		velocity += input_vector * ACCELERATION * delta
 		velocity = velocity.clamped(MAX_SPEED)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
 		#if collision.collider.is_in_group("platforms"):
