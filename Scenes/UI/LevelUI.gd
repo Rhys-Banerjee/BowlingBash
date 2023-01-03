883deaec8c9b5e82dcbbe68112008ad68e5aad68
Rhys <rhys.banerjee@gmail.com>
2023-01-03 17:47:23 -0500

    Timer now displaying numeric value

--- Scenes/UI/LevelUI.gd
+++ Scenes/UI/LevelUI.gd
@@ -9,17 +9,21 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var baseLevels = get_tree().get_nodes_in_group("base_level")
-	
 	if (baseLevels.size() > 0):
 		baseLevels[0].connect("pin_total_changed", self, "on_pin_total_changed")
 
 func on_pin_total_changed(totalPins, collectedPins):
-	$MarginContainer/Label.text = str(totalPins-collectedPins, " PINS LEFT")
+	$HBoxContainer/MarginContainer/Label.text = str(totalPins-collectedPins, " PINS LEFT")
 	$Popups/Label.text = str(totalPins-collectedPins)
 	if collectedPins == 0:
 		$Popups/Label.text = str("GO!")
 	$Popups/Label/AnimationPlayer.play("pinCollected")
+	
+func round_up(f):
+	return int(f) + 1 if f > int(f) else int(f)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var timer = get_parent().get_node("LevelTimer")
+	$HBoxContainer/MarginContainer2/Label.text = str(round_up(timer.time_left), " seconds")
+
