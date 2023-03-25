3077ebfe2f5741c34526dc8ea721eb369eb13ee5
Rhys Banerjee <urb@duke.edu>
2023-03-25 18:13:22 -0400

    Added routing to level select screen in level complete scene

--- Scenes/UI/LevelComplete.gd
+++ Scenes/UI/LevelComplete.gd
@@ -6,13 +6,18 @@ extends CanvasLayer
 # var b = "text"
 
 onready var replay = $PanelContainer/MarginContainer/VBoxContainer/Replay
+onready var exit = $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/ExitButton
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$PanelContainer/MarginContainer/VBoxContainer/NextLevel.connect("pressed", self, "on_next_button_pressed")
 	replay.connect("pressed", self, "on_replay_button_pressed")
+	exit.connect("pressed", self, "on_exit_button_pressed")
 func on_next_button_pressed():
 	$"/root/LevelManager".increment_level()
 	 # Replace with function body.
+
+func on_exit_button_pressed():
+	$"/root/LevelManager".change_level(0)
 
 func on_replay_button_pressed():
 	queue_free()
